fb03fab74f3543a2ee3c1ae8120eceef53c55349
Alek-beep <alek.karoli@gmail.com>
2022-08-05 11:49:48 +1000

    Merge task 5 g On branch addingFunction

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 console.log("Hello World");
-console.log(adder(5,10))
+console.log(adder(5,10));
 function adder(x, y){
     return x + y;
 }
-=======
-console.log("partner changes");
->>>>>>> 638bcb6244e556bdcf13722351ced4801a49fc04
+
+
+
+
